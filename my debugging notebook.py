60570b66956011952ee1752d44922d2937e19b1a
craigng <craigng>
2021-08-10 04:34:00 +0000

    made some updates

---
+++ my debugging notebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+import test_libraries
+
+# COMMAND ----------
+
+modules.
+
+# COMMAND ----------
+
+from modules import test_module
+
+# COMMAND ----------
+
+# MAGIC %autoreload 2
+
+# COMMAND ----------
+
+
